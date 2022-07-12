97a6d4e6b2ffebef57d3aef8954fb5a9570fce58
natr104 <troy.nathan@gmail.com>
2022-07-12 22:02:47 +1000

    created register new user, user index and user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+    
     def create
-        User.create(user_params)
+        @user = User.create(user_params)
+
+        @user.email.downcase!
+
+        if @user.save
+            flash[:notice] = "Account created! Welcome to Dadbook!"
+            redirect_to users_path
+        else
+            flash.now.alert = "Unable to create account - invalid email address or password"
+            render :new
+        end
+
+    end
+
+    def edit
+        @user = User.find(params[:id])
     end
 
     private
