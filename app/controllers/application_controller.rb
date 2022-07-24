5e12ed7ea378074ed9d84e848ab798ce3df77a1e
natr104 <troy.nathan@gmail.com>
2022-07-24 18:51:26 +1000

    redirect to root unless logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     before_action :require_login
 
     def require_login
-        return head(:forbidden) unless session.include? :user_id
+        return redirect_to root_path unless session.include? :user_id
     end
 
     def current_user
